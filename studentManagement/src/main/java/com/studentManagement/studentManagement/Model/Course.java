27513d18ec3cdd1326b243d175fbe1a2e3ca0760
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-28 22:01:45 +0530

    Course entity done

---
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Model/Course.java
@@ -0,0 +1,36 @@
+package com.studentManagement.studentManagement.Model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String courseName;
+
+    public Course(Long id, String courseName) {
+        this.id = id;
+        this.courseName = courseName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName;
+    }
+}
---
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Model/Enrollment.java
@@ -0,0 +1,4 @@
+package com.studentManagement.studentManagement.Model;
+
+public class Enrollment {
+}
