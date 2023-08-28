fd5a1948fe2fc2d739e680581d4cf6a4289f7ae7
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-28 22:21:02 +0530

    Student entity done

--- studentManagement/src/main/java/com/studentManagement/studentManagement/Model/Student.java
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Model/Student.java
@@ -1,4 +1,47 @@
 package com.studentManagement.studentManagement.Model;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Student {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private Long id;
+    private String firstName;
+    private String lastName;
+
+    public Student(Long id, String firstName, String lastName) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
