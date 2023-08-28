ae7099bf6995e6b4f5866e7a941c825225cc4db8
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-28 22:26:38 +0530

    Enrollment entity done

--- studentManagement/src/main/java/com/studentManagement/studentManagement/Model/Enrollment.java
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Model/Enrollment.java
@@ -1,4 +1,22 @@
 package com.studentManagement.studentManagement.Model;
 
+import jakarta.persistence.*;
+
+import java.time.LocalDate;
+
+@Entity
 public class Enrollment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name="student_id")
+    private Student student;
+
+    @ManyToOne
+    @JoinColumn(name="course_id")
+    private Course course;
+
+    private LocalDate enrollmentDate;
 }
