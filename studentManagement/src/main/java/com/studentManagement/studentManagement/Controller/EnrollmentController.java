97759141e9902cb216d21531ccbc098cf74bbc34
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-31 00:25:35 +0530

    EnrollmentController

--- studentManagement/src/main/java/com/studentManagement/studentManagement/Controller/EnrollmentController.java
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Controller/EnrollmentController.java
@@ -1,4 +1,51 @@
 package com.studentManagement.studentManagement.Controller;
 
+import com.studentManagement.studentManagement.Model.Enrollment;
+import com.studentManagement.studentManagement.Repository.EnrollmentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/enrollments")
 public class EnrollmentController {
+    @Autowired
+    private EnrollmentRepository enrollmentRepository;
+
+    @GetMapping
+    public List<Enrollment> getAllEnrollments() {
+        return enrollmentRepository.findAll();
+    }
+
+    @PostMapping
+    public Enrollment createEnrollment(@RequestBody Enrollment enrollment) {
+        return enrollmentRepository.save(enrollment);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Enrollment> updateEnrollment(@PathVariable Long id, @RequestBody Enrollment updatedEnrollment) {
+        Optional<Enrollment> optionalEnrollment = enrollmentRepository.findById(id);
+        if (optionalEnrollment.isPresent()) {
+            Enrollment existingEnrollment = optionalEnrollment.get();
+            existingEnrollment.setEnrollmentDate(updatedEnrollment.getEnrollmentDate());
+            enrollmentRepository.save(existingEnrollment);
+            return ResponseEntity.ok(existingEnrollment);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteEnrollment(@PathVariable Long id) {
+        Optional<Enrollment> optionalEnrollment = enrollmentRepository.findById(id);
+        if (optionalEnrollment.isPresent()) {
+            enrollmentRepository.delete(optionalEnrollment.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
--- studentManagement/src/main/java/com/studentManagement/studentManagement/Model/Enrollment.java
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Model/Enrollment.java
@@ -19,4 +19,36 @@ public class Enrollment {
     private Course course;
 
     private LocalDate enrollmentDate;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    public LocalDate getEnrollmentDate() {
+        return enrollmentDate;
+    }
+
+    public void setEnrollmentDate(LocalDate enrollmentDate) {
+        this.enrollmentDate = enrollmentDate;
+    }
 }
