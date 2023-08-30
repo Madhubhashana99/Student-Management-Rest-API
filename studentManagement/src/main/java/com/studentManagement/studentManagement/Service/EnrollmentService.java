a5b419125019ada18afd859d65589e686a7d7c75
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-30 23:47:04 +0530

    update EnrollmentService

--- studentManagement/src/main/java/com/studentManagement/studentManagement/Service/EnrollmentService.java
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Service/EnrollmentService.java
@@ -1,4 +1,13 @@
 package com.studentManagement.studentManagement.Service;
 
+import com.studentManagement.studentManagement.Model.Enrollment;
+
+import java.util.List;
+
 public interface EnrollmentService {
+    List<Enrollment> getAllEnrollments();
+    Enrollment getEnrollmentById(Long id);
+    Enrollment createEnrollment(Enrollment enrollment);
+    Enrollment updateEnrollment(Long id, Enrollment enrollment);
+    void deleteEnrollment(Long id);
 }
