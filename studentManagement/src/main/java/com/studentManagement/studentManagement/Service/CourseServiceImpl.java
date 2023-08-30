6c1b5c1c38067fd3d860ecc5a3b1962fecf7eef8
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-30 23:56:55 +0530

    CourseServiceImpl

--- studentManagement/src/main/java/com/studentManagement/studentManagement/Service/CourseServiceImpl.java
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Service/CourseServiceImpl.java
@@ -1,4 +1,38 @@
 package com.studentManagement.studentManagement.Service;
 
-public class CourseServiceImpl {
+import com.studentManagement.studentManagement.Model.Course;
+import com.studentManagement.studentManagement.Repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class CourseServiceImpl implements CourseService{
+
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Override
+    public List<Course> getAllCourses() {
+        return null;
+    }
+
+    @Override
+    public Course getCourseById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Course createCourse(Course course) {
+        return null;
+    }
+
+    @Override
+    public Course updateCourse(Long id, Course course) {
+        return null;
+    }
+
+    @Override
+    public void deleteCourse(Long id) {
+
+    }
 }
