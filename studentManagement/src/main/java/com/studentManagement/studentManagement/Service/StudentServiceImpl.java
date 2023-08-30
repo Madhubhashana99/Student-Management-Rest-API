dbf4565fac32fd6373bb14cd0c42525d95d9e543
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-30 23:52:53 +0530

    StudentServiceImpl

---
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Service/StudentServiceImpl.java
@@ -0,0 +1,39 @@
+package com.studentManagement.studentManagement.Service;
+
+import com.studentManagement.studentManagement.Model.Student;
+import com.studentManagement.studentManagement.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class StudentServiceImpl implements StudentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+    
+
+    @Override
+    public List<Student> getAllStudents() {
+        return null;
+    }
+
+    @Override
+    public Student getStudentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Student createStudent(Student student) {
+        return null;
+    }
+
+    @Override
+    public Student updateStudent(Long id, Student student) {
+        return null;
+    }
+
+    @Override
+    public void deleteStudent(Long id) {
+
+    }
+}
