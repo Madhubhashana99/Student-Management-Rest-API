bfe05eca48c8cd85f722e0cf83f1266d0462ecba
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-30 23:55:14 +0530

    EnrollmentServiceImpl

--- studentManagement/src/main/java/com/studentManagement/studentManagement/Service/EnrollmentServiceImpl.java
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Service/EnrollmentServiceImpl.java
@@ -1,4 +1,38 @@
 package com.studentManagement.studentManagement.Service;
 
-public class EnrollmentServiceImpl {
+import com.studentManagement.studentManagement.Model.Enrollment;
+import com.studentManagement.studentManagement.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class EnrollmentServiceImpl implements EnrollmentService{
+
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @Override
+    public List<Enrollment> getAllEnrollments() {
+        return null;
+    }
+
+    @Override
+    public Enrollment getEnrollmentById(Long id) {
+        return null;
+    }
+
+    @Override
+    public Enrollment createEnrollment(Enrollment enrollment) {
+        return null;
+    }
+
+    @Override
+    public Enrollment updateEnrollment(Long id, Enrollment enrollment) {
+        return null;
+    }
+
+    @Override
+    public void deleteEnrollment(Long id) {
+
+    }
 }
--- studentManagement/src/main/java/com/studentManagement/studentManagement/Service/StudentServiceImpl.java
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Service/StudentServiceImpl.java
@@ -10,7 +10,7 @@ public class StudentServiceImpl implements StudentService{
 
     @Autowired
     private StudentRepository studentRepository;
-    
+
 
     @Override
     public List<Student> getAllStudents() {
