2978c82f82ecc2fa0080f91c668720941fc17eeb
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-31 00:11:29 +0530

    Update CourseController

--- studentManagement/src/main/java/com/studentManagement/studentManagement/Controller/CourseController.java
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Controller/CourseController.java
@@ -1,4 +1,51 @@
 package com.studentManagement.studentManagement.Controller;
 
+import com.studentManagement.studentManagement.Model.Course;
+import com.studentManagement.studentManagement.Repository.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/courses")
 public class CourseController {
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    @PostMapping
+    public Course createCourse(@RequestBody Course course) {
+        return courseRepository.save(course);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Course> updateCourse(@PathVariable Long id, @RequestBody Course updatedCourse) {
+        Optional<Course> optionalCourse = courseRepository.findById(id);
+        if (optionalCourse.isPresent()) {
+            Course existingCourse = optionalCourse.get();
+            existingCourse.setCourseName(updatedCourse.getCourseName());
+            courseRepository.save(existingCourse);
+            return ResponseEntity.ok(existingCourse);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCourse(@PathVariable Long id) {
+        Optional<Course> optionalCourse = courseRepository.findById(id);
+        if (optionalCourse.isPresent()) {
+            courseRepository.delete(optionalCourse.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
