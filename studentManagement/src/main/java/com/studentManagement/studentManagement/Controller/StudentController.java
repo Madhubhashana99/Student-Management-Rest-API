51638a182285bdbb51b6cb17dd99028a4d04bd5a
Madhubhashana99 <104136158+Madhubhashana99@users.noreply.github.com>
2023-08-31 00:07:44 +0530

    Update StudentController

--- studentManagement/src/main/java/com/studentManagement/studentManagement/Controller/StudentController.java
+++ studentManagement/src/main/java/com/studentManagement/studentManagement/Controller/StudentController.java
@@ -1,5 +1,54 @@
 package com.studentManagement.studentManagement.Controller;
 
-public class StudentController {
+import com.studentManagement.studentManagement.Model.Student;
+import com.studentManagement.studentManagement.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/api/students")
+public class StudentController {
+    @Autowired
+    private StudentRepository studentRepository;
+
+    @GetMapping
+    public List<Student> getAllStudents() {
+        return studentRepository.findAll();
+    }
+
+    @PostMapping
+    public Student createStudent(@RequestBody Student student) {
+        return studentRepository.save(student);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Student> updateStudent(@PathVariable Long id, @RequestBody Student updatedStudent) {
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+        if (optionalStudent.isPresent()) {
+            Student existingStudent = optionalStudent.get();
+            existingStudent.setFirstName(updatedStudent.getFirstName());
+            existingStudent.setLastName(updatedStudent.getLastName());
+            // Set other properties as needed
+            studentRepository.save(existingStudent);
+            return ResponseEntity.ok(existingStudent);
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id) {
+        Optional<Student> optionalStudent = studentRepository.findById(id);
+        if (optionalStudent.isPresent()) {
+            studentRepository.delete(optionalStudent.get());
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
 }
+
